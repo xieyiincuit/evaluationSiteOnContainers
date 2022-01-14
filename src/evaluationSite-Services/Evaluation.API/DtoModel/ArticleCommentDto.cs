982ebfc1fd32ef3a3f406ee80ae0be831d2d7be0
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-14 18:21:47 +0800

    [#add] preper for comment and reply function

---
+++ src/evaluationSite-Services/Evaluation.API/DtoModel/ArticleCommentDto.cs
@@ -0,0 +1,30 @@
+﻿namespace Zhouxieyi.evalutionSiteOnContainers.Services.Evaluation.API.DtoModel;
+
+public class ArticleCommentDto
+{
+    public int CommentId { get; set; }
+    public int UserId { get; set; }
+    public string NickName { get; set; }
+    public string? Avatar { get; set; }
+    public string Content { get; set; }
+    public int SupportCount { get; set; }
+    public DateTime CreateTime { get; set; }
+    public int RepliesCount { get; set; }
+
+    public List<ReplyCommentDto> Replies { get; set; }
+}
+
+public class ReplyCommentDto
+{
+    public int CommentId { get; set; }
+    public int UserId { get; set; }
+    public string NickName { get; set; }
+    public string? Avatar { get; set; }
+    public string ReplyContent { get; set; }
+    public int SupportCount { get; set; }
+    public DateTime CreateTime { get; set; }
+
+    public List<ReplyCommentDto> Replies { get; set; }
+    public int ReplyCommentId { get; set; }
+    public string ReplyNickName { get; set; }
+}
