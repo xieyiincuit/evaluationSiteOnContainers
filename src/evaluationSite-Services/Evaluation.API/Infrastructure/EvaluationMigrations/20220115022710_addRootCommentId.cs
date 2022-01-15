7be5f588353d8020eb677c04025c7e9938207fb4
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-15 10:46:35 +0800

    [#migarions] Add "RootCommentId" property on comment table

---
+++ src/evaluationSite-Services/Evaluation.API/Infrastructure/EvaluationMigrations/20220115022710_addRootCommentId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evaluation.API.Infrastructure.EvaluationMigrations
+{
+    public partial class addRootCommentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "root_comment_id",
+                table: "evaluation_comment",
+                type: "int",
+                nullable: true,
+                comment: "回复评论属于哪个主评论");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "root_comment_id",
+                table: "evaluation_comment");
+        }
+    }
+}
