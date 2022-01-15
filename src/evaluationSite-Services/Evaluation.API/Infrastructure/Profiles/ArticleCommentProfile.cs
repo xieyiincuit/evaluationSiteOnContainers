9b9f2c11dfcff4e7cb9c980ec466ccebe999947e
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-15 10:47:59 +0800

    [#add] add replyCommentDto mapping

--- src/evaluationSite-Services/Evaluation.API/Infrastructure/Profiles/ArticleCommentProfile.cs
+++ src/evaluationSite-Services/Evaluation.API/Infrastructure/Profiles/ArticleCommentProfile.cs
@@ -5,5 +5,6 @@ public class ArticleCommentProfile : Profile
     public ArticleCommentProfile()
     {
         CreateMap<EvaluationComment, ArticleCommentDto>();
+        CreateMap<EvaluationComment, ReplyCommentDto>();
     }
 }
