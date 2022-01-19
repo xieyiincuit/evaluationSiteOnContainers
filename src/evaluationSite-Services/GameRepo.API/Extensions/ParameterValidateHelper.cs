f1ecf2a673ab6108b7e806f2cb376abcb247bb21
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-19 17:58:09 +0800

    [#add] pageParameter check tools

---
+++ src/evaluationSite-Services/GameRepo.API/Extensions/ParameterValidateHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Zhouxieyi.evalutionSiteOnContainers.Services.GameRepo.API.Extensions;
+
+public static class ParameterValidateHelper
+{
+    public static bool IsInvalidPageIndex(int totalCount, int pageSize, int pageIndex)
+    {
+        if (pageIndex <= 0) return true;
+
+        var maxPageIndex = (int)Math.Ceiling(totalCount / (double)pageSize);
+        return pageIndex > maxPageIndex;
+    }
+}
