f5c2434e14e59ae7c1119142c48868e71e8b4bf3
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-19 17:57:51 +0800

    [#add] add mapping profile

---
+++ src/evaluationSite-Services/GameRepo.API/Infrastructure/Profiles/CategoryProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Zhouxieyi.evalutionSiteOnContainers.Services.GameRepo.API.Infrastructure.Profiles;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<GameCategoryAddDto, GameCategory>();
+        CreateMap<GameCategoryUpdateDto, GameCategory>();
+    }
+}
---
+++ src/evaluationSite-Services/GameRepo.API/Infrastructure/Profiles/CompanyProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Zhouxieyi.evalutionSiteOnContainers.Services.GameRepo.API.Infrastructure.Profiles;
+
+public class CompanyProfile : Profile
+{
+    public CompanyProfile()
+    {
+        CreateMap<GameCompanyAddDto, GameCompany>();
+        CreateMap<GameCompanyUpdateDto, GameCompany>();
+    }
+}
