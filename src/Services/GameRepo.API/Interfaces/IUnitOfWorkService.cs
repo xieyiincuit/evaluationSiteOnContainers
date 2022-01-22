3acba671f725688649eb5ba2e4a3c34fb5641150
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-01-22 17:19:30 +0800

    [#add] unitofwork

---
+++ src/Services/GameRepo.API/Interfaces/IUnitOfWorkService.cs
@@ -0,0 +1,7 @@
+﻿namespace Zhouxieyi.evaluationSiteOnContainers.Services.GameRepo.API.Interfaces;
+
+public interface IUnitOfWorkService
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Services/GameRepo.API/Services/UnitOfWorkService.cs
@@ -0,0 +1,21 @@
+﻿namespace Zhouxieyi.evaluationSiteOnContainers.Services.GameRepo.API.Services;
+
+public class UnitOfWorkService : IUnitOfWorkService
+{
+    private readonly GameRepoContext _repoContext;
+
+    public UnitOfWorkService(GameRepoContext repoContext)
+    {
+        _repoContext = repoContext ?? throw new ArgumentNullException(nameof(repoContext));
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _repoContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _repoContext.SaveChangesAsync(cancellationToken) > 0;
+    }
+}
