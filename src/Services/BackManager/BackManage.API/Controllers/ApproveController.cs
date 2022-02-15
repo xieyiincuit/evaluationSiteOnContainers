5116e27e50ba2799feeda84b2d9087eae1f1a210
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-02-15 14:04:01 +0800

    【#update】 function init

---
+++ src/Services/BackManager/BackManage.API/Controllers/ApproveController.cs
@@ -0,0 +1,26 @@
+﻿namespace BackManage.API.Controllers;
+
+[ApiController]
+[Route("api/v1/b")]
+public class ApproveController : ControllerBase
+{
+    private readonly ILogger<ApproveController> _logger;
+
+    public ApproveController(ILogger<ApproveController> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+
+    [HttpPost("approve/{userId:alpha}")]
+    public async Task<IActionResult> ApprovedUserToEvaluatorAsync([FromRoute] string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("band/{userId:alpha}")]
+    public async Task<IActionResult> BandUserAsync([FromRoute] string userId)
+    {
+        throw new NotImplementedException();
+    }
+}
