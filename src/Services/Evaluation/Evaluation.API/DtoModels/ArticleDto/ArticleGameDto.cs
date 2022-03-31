485717129490039af5ee01a61557bfca968e0720
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-03-31 17:21:23 +0800

    [#update] 评论模块完善

---
+++ src/Services/Evaluation/Evaluation.API/DtoModels/ArticleDto/ArticleGameDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Zhouxieyi.evaluationSiteOnContainers.Services.Evaluation.API.DtoModels;
+public class ArticleGameDto
+{
+    public int ArticleId { get; set; }
+    public string AuthorId { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public int CommentsCount { get; set; }
+    public int SupportCount { get; set; }
+    public DateTime CreateTime { get; set; }
+}
