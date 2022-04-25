89f8902fb7a7cb854a748af08cc25cdc419081b3
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-04-25 18:33:13 +0800

    [#update] 电话号码验证

--- src/Services/IdentityServer/Identity.API/Models/ViewModels/Login/RegisterViewModel.cs
+++ src/Services/IdentityServer/Identity.API/Models/ViewModels/Login/RegisterViewModel.cs
@@ -2,22 +2,22 @@
 
 public class RegisterViewModel
 {
+    [Display(Name = "邮箱")]
     [Required(ErrorMessage = "请填写邮箱")]
     [EmailAddress]
-    [Display(Name = "邮箱")]
     public string Email { get; init; }
 
     [Display(Name = "电话号码")]
-    [Phone]
     [Required(ErrorMessage = "请填写你的电话号码")]
-    public string PhoneNumber { get; set; }
+    [RegularExpression(@"^1[0-9]{10}$", ErrorMessage = "请输入合法的电话号码")]
+    public string PhoneNumber { get; init; }
 
+    [Display(Name = "密码")]
     [Required(ErrorMessage = "请填写密码")]
     [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,30}$"
         , ErrorMessage = "至少8个字符，至少1个字母，1个数字和1个特殊字符(@$!%*#?&)")]
     [StringLength(30, ErrorMessage = " {0}的长度在{2}到{1}之间.", MinimumLength = 8)]
     [DataType(DataType.Password)]
-    [Display(Name = "密码")]
     public string Password { get; init; }
 
     [DataType(DataType.Password)]
